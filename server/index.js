c3941091ba0efc9c0b18659c881392c83bf87e78
Shubham <shubhamgond7678@gmail.com>
2025-07-09 00:24:58 +0530

    connect to database

--- server/index.js
+++ server/index.js
@@ -1,6 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 const app=express();
+
+mongoose.connect(process.env.MONGODB_URL)
+.then(()=>{
+    console.log("Connected To Database!")
+})
+.catch((err)=>{
+    console.log(err);
+})
 
 app.listen(3000,()=>{
     console.log("Server is listening at port 3000..");
